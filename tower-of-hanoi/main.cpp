67bac2826447ecc43fa40b17c9e660c5e2ebc2fe
ijwtem <fakkar.keriting@gmail.com>
2021-07-15 13:31:17 +0700

    Looping after game finished

--- tower-of-hanoi/main.cpp
+++ tower-of-hanoi/main.cpp
@@ -7,29 +7,31 @@
 
 
 int main(){
-	bool is_play = false;
-	char* menu_awal[] = {">> Play Game\n", ">> How To Play\n", ">> Scoreboard\n", ">> Exit Game\n"};
-	Menu MainMenu(4, menu_awal);
 	
-	
-	while(!is_play){
-		
-		system("cls");
-		printf("Menu\n");
-		MainMenu.print();
-		int key = _getch();
-		if (key == 224) {
-			switch(_getch()) { // the real value
+	while (true) {
+		bool is_play = false;
+		char* menu_awal[] = { ">> Play Game\n", ">> How To Play\n", ">> Scoreboard\n", ">> Exit Game\n" };
+		Menu MainMenu(4, menu_awal);
+
+
+		while (!is_play) {
+
+			system("cls");
+			printf("Menu\n");
+			MainMenu.print();
+			int key = _getch();
+			if (key == 224) {
+				switch (_getch()) { // the real value
 				case 72: //up
 					MainMenu.up();
 					break;
 				case 80: //down
 					MainMenu.down();
 					break;
+				}
 			}
-		} 
-		else if (key == 13){ //if enter key pressed
-			switch(MainMenu.get()){
+			else if (key == 13) { //if enter key pressed
+				switch (MainMenu.get()) {
 				case 1:
 					is_play = true;
 					break;
@@ -42,66 +44,68 @@ int main(){
 				case 4:
 					exit(0);
 					break;
+				}
+			}
+			else if (key == 32) { //if space key pressed
+				MainMenu.select();
 			}
 		}
-		else if (key == 32){ //if space key pressed
-			MainMenu.select();
-		}
-	}
 
-	Game myGame("test_name",3);
-	
-	char* tower_name[3];
-	tower_name[0] = (char*)malloc(40);
-	tower_name[1] = (char*)malloc(40);
-	tower_name[2] = (char*)malloc(40);
-	sprintf(tower_name[0], "%*sTower 1%*s", myGame.getDifficulty()-2, "", myGame.getDifficulty()-2, "");
-	sprintf(tower_name[1], "%*sTower 2%*s", myGame.getDifficulty()-2, "", myGame.getDifficulty()-2, "");
-	sprintf(tower_name[2], "%*sTower 3%*s", myGame.getDifficulty()-2, "", myGame.getDifficulty()-2, "");
+		Game myGame("test_name", 3);
+
+		char* tower_name[3];
+		tower_name[0] = (char*)malloc(40);
+		tower_name[1] = (char*)malloc(40);
+		tower_name[2] = (char*)malloc(40);
+		sprintf(tower_name[0], "%*sTower 1%*s", myGame.getDifficulty() - 2, "", myGame.getDifficulty() - 2, "");
+		sprintf(tower_name[1], "%*sTower 2%*s", myGame.getDifficulty() - 2, "", myGame.getDifficulty() - 2, "");
+		sprintf(tower_name[2], "%*sTower 3%*s", myGame.getDifficulty() - 2, "", myGame.getDifficulty() - 2, "");
 
 
-	Menu towerMenu(3, tower_name, 2);
-	free(tower_name[0]);
-	free(tower_name[1]);
-	free(tower_name[2]);
+		Menu towerMenu(3, tower_name, 2);
+		free(tower_name[0]);
+		free(tower_name[1]);
+		free(tower_name[2]);
 
-	while(true){
-		system("cls");
-		myGame.print();
-		towerMenu.print();
-		if(myGame.isFinished())
-			break;
-		printf("\n");
-		int *selected_tower = towerMenu.get_all();
-		for(int i = 1; i<4; i++){
-			if(i == selected_tower[0])
-				printf(" %*sF%*s ", myGame.getDifficulty(), "", myGame.getDifficulty(), "");
-			else if(i == selected_tower[1])
-				printf(" %*sT%*s ", myGame.getDifficulty(), "", myGame.getDifficulty(), "");
-			else
-				printf(" %*s %*s ", myGame.getDifficulty(), "", myGame.getDifficulty(), "");
-		}
+		while (true) {
+			system("cls");
+			myGame.print();
+			towerMenu.print();
+			if (myGame.isFinished())
+				break;
+			printf("\n");
+			int* selected_tower = towerMenu.get_all();
+			for (int i = 1; i < 4; i++) {
+				if (i == selected_tower[0])
+					printf(" %*sF%*s ", myGame.getDifficulty(), "", myGame.getDifficulty(), "");
+				else if (i == selected_tower[1])
+					printf(" %*sT%*s ", myGame.getDifficulty(), "", myGame.getDifficulty(), "");
+				else
+					printf(" %*s %*s ", myGame.getDifficulty(), "", myGame.getDifficulty(), "");
+			}
 
-		int key = _getch();
-		if (key == 224) {
-			switch(_getch()) { // the real value
+			int key = _getch();
+			if (key == 224) {
+				switch (_getch()) { // the real value
 				case 75: //left
 					towerMenu.up();
 					break;
 				case 77: //right
 					towerMenu.down();
 					break;
+				}
 			}
-		} 
-		else if (key == 13){ //if enter key pressed
-			if(myGame.move(selected_tower[0], selected_tower[1]))
-				towerMenu.reset();
+			else if (key == 13) { //if enter key pressed
+				if (myGame.move(selected_tower[0], selected_tower[1]))
+					towerMenu.reset();
+			}
+			else if (key == 32) { //if space key pressed
+				towerMenu.select();
+			}
 		}
-		else if (key == 32){ //if space key pressed
-			towerMenu.select();
-		}
+		printf("\n%d", myGame.getStep());
+		system("pause");
 	}
-	printf("\n%d", myGame.getStep());
-	system("pause");
+	
 	return 0;
 }
